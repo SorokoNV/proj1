2a177219ecca2103ed94072e08cc3a51b4d9c704
SorokoNV <soroko_nikolai@mail.ru>
2024-08-13 17:34:26 +0300

    Добавлен вызов метода doSomething()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("---");
         System.out.println("Hello World!");
+        doSomething();
         System.out.println("3343");
     }
 
